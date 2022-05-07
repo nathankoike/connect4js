6f63a4c81ac9796d5f4a75e918edade4da91ba4b
Nate Koike <nkoike1998@gmail.com>
2022-05-06 20:59:09 -1000

    finished game(maybe)

--- game.js
+++ game.js
@@ -11,6 +11,71 @@ class Game {
     this.turn = 0;
   }
 
+  // Reset the game
+  _reset() {
+    // Reset the board
+    for (let c = 0; c < this.board.length; c++)
+      this.board[c] = Array(this.board[0].length).fill(" ");
+
+    // Reset the turn counter
+    this.turn = 0;
+  }
+
+  // Determine if a move is valid
+  _isMoveValid(column) {
+    // Does the column still have an empty space?
+    return this.board[column].indexOf(" ") >= 0;
+  }
+
+  // Determine if the game is over
+  _isGameOver(column) {
+    // Every direction in which a player could have a match
+    const dirs = [
+      [1, 0],
+      [0, 1],
+      [1, 1],
+      [-1, 1],
+    ];
+
+    // Search every direction
+    for (let i = 0; i < dirs.length; i++) {
+      // Find the coordinates of the placed piece
+      let coords = [column, this.board[column].indexOf(" ") - 1];
+
+      // Make sure the starting coordinates are within the board
+      if (coords[1] < 0) coords[1] = this.board[0].length - 1;
+
+      // Move to one extreme of the board from the piece coordinates in the
+      // current direction
+      for (
+        ;
+        coords[0] - dirs[i][0] >= 0 &&
+        coords[1] - dirs[i][1] >= 0 &&
+        coords[0] < this.board.length &&
+        coords[1] < this.board[0].length;
+        coords = [coords[0] - dirs[i][0], coords[1] - dirs[i][1]]
+      );
+
+      // Build the string
+      let line = "";
+
+      for (
+        ;
+        coords[0] >= 0 &&
+        coords[1] >= 0 &&
+        coords[0] < this.board.length &&
+        coords[1] < this.board[0].length;
+        coords = [coords[0] + dirs[i][0], coords[1] + dirs[i][1]]
+      )
+        line += this.board[coords[0]][coords[1]];
+
+      // If the line has four in a row, we can end here
+      if (line.includes(this.players[this.turn % 2].repeat(4))) return true;
+    }
+
+    return false;
+  }
+
   // Print the board to the screen
   printBoard() {
     for (let r = this.board[0].length - 1; r >= 0; r--) {
@@ -22,22 +87,11 @@ class Game {
     }
   }
 
-  // Determine if a move is valid
-  _isMoveValid(column) {
-    // Does the column still have an empty space?
-    return this.board[column].indexOf(" ") >= 0;
-  }
-
-  // Determine if the game is over
-  isGameOver() {
-    return false;
-  }
-
   // Place a player's piece and increment the turn count
   move(column) {
     // If the user had tried to enter an invalid move
-    if (!this.isMoveValid(column)) {
-      console.log("Enter a valid move\n");
+    if (!this._isMoveValid(column)) {
+      this.printBoard();
       return;
     }
 
@@ -48,12 +102,16 @@ class Game {
       return e;
     });
 
+    if (this._isGameOver(column)) {
+      console.log(`Player ${(this.turn % 2) + 1} has won!`);
+      this.printBoard();
+      this._reset();
+      return this.turn % 2;
+    }
+
     this.turn++;
+    return " ";
   }
 }
 
 let x = new Game(standardWidth, standardHeight);
-
-// x.printBoard();
-
-x.printBoard();
